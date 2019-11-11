e9bd2fc568b1eb98ab5b3c97820ae2214435dd20
EloinaCarrillo <elodinda04@gmail.com>
2019-11-11 14:15:03 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
-
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
